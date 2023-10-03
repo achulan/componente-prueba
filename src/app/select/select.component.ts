38eee53ce7b3f94c414ea999acba100a393e6e4c
Anastasia Prischep <a.chulanikova@gmail.com>
2023-10-03 20:00:01 +0200

    On Push

--- src/app/select/select.component.ts
+++ src/app/select/select.component.ts
@@ -1,9 +1,10 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { ChangeDetectionStrategy, Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-select',
   templateUrl: './select.component.html',
-  styleUrls: ['./select.component.css']
+  styleUrls: ['./select.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 
 export class SelectComponent {
